9f5c664a411dc261cdabef38976fce1148faa69e
Augustine Okeke <hursteen@gmail.com>
2020-01-23 20:20:12 +0100

    added the normalize email helper function to our test and model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,6 +13,13 @@ class ModelTests(TestCase):
         email=email,
         password=password
     )
-    
+
     self.assertEqual(user.email, email)
     self.assertTrue(user.check_password(password))
+
+
+  def test_new_user_email_normalized(self):
+    """Test the email for a new user is normalized"""
+    email = "test@gmail.com"
+    user = get_user_model().objects.create_user(email, 'austin12345')
+    self.assertEqual(user.email, email.lower())
